7a244cf6921a512856309a62ed759d62bcfb0ec1
Matcha-Teaa <danielmatcha33@gmail.com>
2023-05-07 17:27:22 -0400

    should work

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-latitudes = [33.93911, 41.153332, 28.033886, -11.202692, -38.416097, 40.069099, -25.274398, 47.516231, 40.143105, 25.03428, 26.0667, 23.684994, 13.193887, 53.709807, 50.503887, 17.189877, 9.30769, 27.514162, -16.290154, 43.915886, -22.328474, -14.235004, 4.535277, 42.733883, 12.238333, -3.373056, 16.002082, 12.565679, 7.369722, 56.130366, 6.611111, 15.454166, -35.675147, 35.86166, 4.570868, -11.875001, -4.038333, -0.228021, 9.748917, 7.539989, 45.1, 21.521757, 35.126413, 49.817492, 56.26392, 11.825138, 18.735693, -1.8312, 26.8206, 13.7942, 1.6508, 15.1794, 58.5953, -26.5225, 9.1450, -17.7134, 61.9241, 46.2276, -0.8037, 13.4432, 42.3154, 51.1657, 7.9465, 39.0742, 15.7835, 9.9456, 11.8037, 4.8604, 18.9712, 15.2000, 47.1625, 64.9631, 20.5937, -0.7893, 32.4279, 33.2232, 53.1424, 31.0461, 41.8719, 18.1096, 36.2048, 30.5852, 48.0196, 0.0236, 40.3399, 35.9078, 42.6026, 29.3117, 41.2044, 19.8563, 56.8796, 33.8547, -29.61, 6.4281, 26.3351, 54.6872, 49.8153, 41.6086, -18.7669, -13.2543, 4.2105, 3.2028, 17.5707, 35.9375, 21.0079, -20.3484, 23.6345, 47.4116, 46.8625, 42.7087, 31.7917, -18.6657, 21.9162, -22.9576, 28.3949, 52.1326, -40.9006, 12.8654, 17.6078, 9.082, 60.472, 21.4735, 30.3753, 8.5379, -6.314, -23.4425, -9.19, 12.8797, 51.9194, 39.3999, 25.3548, 45.9432, 61.524, -1.9403, 23.8859, 14.4974, 44.0165, 8.4606, 1.3521, 48.669, 46.1512, -9.6457, 5.1521, -30.5595, 6.877, 40.4168, 7.8731, 12.8628, 3.9193, 60.1282, 46.8182, 34.8021, 23.6978, 38.861, -6.369, 15.87, -8.8742, 8.6195, 10.6918, 33.8869, 38.9637, 38.9697, 1.3733, 48.3794, 23.4241, 55.3781, 37.0902, -32.5228, 41.3775, 6.4238, 14.0583, 15.5527, -13.1339, -19.0154]
-longitudes = [67.709953, 20.168331, 1.659626, 17.873887, -63.616672, 45.038189, 133.775136, 14.550072, 47.576927, -77.39628, 50.5577, 90.356331, -59.543198, 27.953389, 4.469936, -88.49765, 2.315834, 90.433601, -63.588653, 17.679076, 24.684866, -51.92528, 114.727669, 25.48583, -1.561593, 29.918886, -24.013197, 104.990963, 12.354722, -106.346771, 20.939444, 18.732207, -71.542969, 104.195397, -74.297333, 43.872219, 21.758664, 15.827659, -83.753428, -5.54708, 15.2, -77.781167, 33.429859, 15.472962, 9.501785, 42.590275, -70.162651, -78.1834, 30.8025, -88.8965, 10.2679, 39.7823, 25.0136, 31.4659, 40.4897, 178.0650, 25.7482, 2.2137, 11.6094, -15.3101, 43.3569, 10.4515, -1.0232, 21.8243, -90.2308, -9.6966, -15.1804, -58.9302, -72.2852, -86.2419, 19.5033, -19.0208, 78.9629, 113.9213, 53.6880, 43.6793, -7.6921, 34.8516, 12.5674, -77.2975, 138.2529, 36.2384, 66.9237, 37.9062, 127.5101, 127.7669, 20.9030, 47.4818, 74.7661, 102.4955, 24.6032, 35.8623, 28.2336, -9.4295, 17.2283, 25.2797, 6.1296, 21.7453, 46.8691, 34.3015, 101.9758, 73.2207, -3.9962, 14.3754, -10.9408, 57.5522, -102.5528, 28.3699, 103.8467, 19.3744, -7.0926, 35.5296, 95.956, 18.4904, 84.124, 5.2913, 174.886, -85.2072, 8.0817, 8.6753, 8.4689, 55.9754, 69.3451, -80.7821, 143.9555, -58.4438, -75.0152, 121.774, 19.1451, -8.2245, 51.1839, 24.9668, 105.3188, 29.8739, 45.0792, -14.4524, 21.0059, -11.7799, 103.8198,  19.699, 14.9955, 160.1562, 46.1996, 22.9375, 31.307, -3.7038, 80.7718, 30.2176, -56.0278, 18.6435, 8.2275, 38.9968, 120.9605, 71.2761, 34.8888, 100.9925, 125.7275, 0.8248, -61.2225, 9.5375, 35.2433, 59.5563, 32.2903, 31.1656, 53.8478, -3.436, -95.7129, -55.7658, 64.5853, -66.5897, 108.2772, 48.5164, 27.8493, 29.1549]
-columns = ['ISO Country code', 'population', 'GDP \n($ USD billions PPP)', 'GDP per capita in $ (PPP)', 'health expenditure \n% of GDP', 'health expenditure \nper person', 'unemployment (%)','Military Spending as % of GDP']
+const latitudes = [33.93911, 41.153332, 28.033886, -11.202692, -38.416097, 40.069099, -25.274398, 47.516231, 40.143105, 25.03428, 26.0667, 23.684994, 13.193887, 53.709807, 50.503887, 17.189877, 9.30769, 27.514162, -16.290154, 43.915886, -22.328474, -14.235004, 4.535277, 42.733883, 12.238333, -3.373056, 16.002082, 12.565679, 7.369722, 56.130366, 6.611111, 15.454166, -35.675147, 35.86166, 4.570868, -11.875001, -4.038333, -0.228021, 9.748917, 7.539989, 45.1, 21.521757, 35.126413, 49.817492, 56.26392, 11.825138, 18.735693, -1.8312, 26.8206, 13.7942, 1.6508, 15.1794, 58.5953, -26.5225, 9.1450, -17.7134, 61.9241, 46.2276, -0.8037, 13.4432, 42.3154, 51.1657, 7.9465, 39.0742, 15.7835, 9.9456, 11.8037, 4.8604, 18.9712, 15.2000, 47.1625, 64.9631, 20.5937, -0.7893, 32.4279, 33.2232, 53.1424, 31.0461, 41.8719, 18.1096, 36.2048, 30.5852, 48.0196, 0.0236, 40.3399, 35.9078, 42.6026, 29.3117, 41.2044, 19.8563, 56.8796, 33.8547, -29.61, 6.4281, 26.3351, 54.6872, 49.8153, 41.6086, -18.7669, -13.2543, 4.2105, 3.2028, 17.5707, 35.9375, 21.0079, -20.3484, 23.6345, 47.4116, 46.8625, 42.7087, 31.7917, -18.6657, 21.9162, -22.9576, 28.3949, 52.1326, -40.9006, 12.8654, 17.6078, 9.082, 60.472, 21.4735, 30.3753, 8.5379, -6.314, -23.4425, -9.19, 12.8797, 51.9194, 39.3999, 25.3548, 45.9432, 61.524, -1.9403, 23.8859, 14.4974, 44.0165, 8.4606, 1.3521, 48.669, 46.1512, -9.6457, 5.1521, -30.5595, 6.877, 40.4168, 7.8731, 12.8628, 3.9193, 60.1282, 46.8182, 34.8021, 23.6978, 38.861, -6.369, 15.87, -8.8742, 8.6195, 10.6918, 33.8869, 38.9637, 38.9697, 1.3733, 48.3794, 23.4241, 55.3781, 37.0902, -32.5228, 41.3775, 6.4238, 14.0583, 15.5527, -13.1339, -19.0154]
+const longitudes = [67.709953, 20.168331, 1.659626, 17.873887, -63.616672, 45.038189, 133.775136, 14.550072, 47.576927, -77.39628, 50.5577, 90.356331, -59.543198, 27.953389, 4.469936, -88.49765, 2.315834, 90.433601, -63.588653, 17.679076, 24.684866, -51.92528, 114.727669, 25.48583, -1.561593, 29.918886, -24.013197, 104.990963, 12.354722, -106.346771, 20.939444, 18.732207, -71.542969, 104.195397, -74.297333, 43.872219, 21.758664, 15.827659, -83.753428, -5.54708, 15.2, -77.781167, 33.429859, 15.472962, 9.501785, 42.590275, -70.162651, -78.1834, 30.8025, -88.8965, 10.2679, 39.7823, 25.0136, 31.4659, 40.4897, 178.0650, 25.7482, 2.2137, 11.6094, -15.3101, 43.3569, 10.4515, -1.0232, 21.8243, -90.2308, -9.6966, -15.1804, -58.9302, -72.2852, -86.2419, 19.5033, -19.0208, 78.9629, 113.9213, 53.6880, 43.6793, -7.6921, 34.8516, 12.5674, -77.2975, 138.2529, 36.2384, 66.9237, 37.9062, 127.5101, 127.7669, 20.9030, 47.4818, 74.7661, 102.4955, 24.6032, 35.8623, 28.2336, -9.4295, 17.2283, 25.2797, 6.1296, 21.7453, 46.8691, 34.3015, 101.9758, 73.2207, -3.9962, 14.3754, -10.9408, 57.5522, -102.5528, 28.3699, 103.8467, 19.3744, -7.0926, 35.5296, 95.956, 18.4904, 84.124, 5.2913, 174.886, -85.2072, 8.0817, 8.6753, 8.4689, 55.9754, 69.3451, -80.7821, 143.9555, -58.4438, -75.0152, 121.774, 19.1451, -8.2245, 51.1839, 24.9668, 105.3188, 29.8739, 45.0792, -14.4524, 21.0059, -11.7799, 103.8198,  19.699, 14.9955, 160.1562, 46.1996, 22.9375, 31.307, -3.7038, 80.7718, 30.2176, -56.0278, 18.6435, 8.2275, 38.9968, 120.9605, 71.2761, 34.8888, 100.9925, 125.7275, 0.8248, -61.2225, 9.5375, 35.2433, 59.5563, 32.2903, 31.1656, 53.8478, -3.436, -95.7129, -55.7658, 64.5853, -66.5897, 108.2772, 48.5164, 27.8493, 29.1549]
 
 /*
 this function cleans the data... replaces each '-' and '' in dataset with 0
@@ -8,8 +7,8 @@ This function also adds the lat and long columns of every country
 */
 
 let current_country = null;
-let myChart;
-
+let current_metric = 0;
+let myChart = null;
 
 const loadData = async() => {
     let df = await d3.csv('data.csv');
@@ -19,14 +18,14 @@ const loadData = async() => {
         if (row[key] === '-' || row[key] === '' || row[key] === '...') {
           row[key] = '0';
         }
-        if (typeof row[key] === 'string') {
-          row[key] = row[key].replace(/,/g, ''); // remove commas from string values
+        if (row[key].includes('%')) {
+          row[key] = row[key].replace('%', '');
         }
-        
       }
       // add column name here as follows to convert to float
       row['population'] = parseFloat(row['population']);
-      // row['health expenditure \nper person'] = parseFloat(row['health expenditure \nper person']);
+      row['Military Spending as % of GDP'] = parseFloat(row['Military Spending as % of GDP']);
+      row['health expenditure \nper person'] = parseInt(row['health expenditure \nper person']);
       row['health expenditure \n% of GDP'] = parseFloat(row['health expenditure \n% of GDP']);
       row["GDP \n($ USD billions PPP)"] = parseFloat(row["GDP \n($ USD billions PPP)"]);
       row["lat"] = latitudes[dex]
@@ -35,7 +34,7 @@ const loadData = async() => {
     });
 
     // uncomment this to get a csv of exactly what the data looks like after formatting
-    
+    /*
     const csvData = d3.csvFormat(df);
     const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
@@ -46,13 +45,13 @@ const loadData = async() => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    
+    */
 
     return df;
 }
   
-// prints data to console
-loadData().then(df => { console.log(df) })
+// prints data array to console
+// loadData().then(df => { console.log(df) })
 
 
 // generates the leaflet map and plots all the points
@@ -92,12 +91,9 @@ const makeMap = async() => {
             if (chartContainer.classList.contains('hide')) {
               chartContainer.classList.remove('hide');
             }
-            
-
-
             titleSelect.textContent = `${row["indicator"]}`
             textSelect.innerHTML = `<b>ISO Country code: </b>${row["ISO Country code"]}<br><b>population: </b>${row["population"]}<br><b>GDP ($ USD billions PPP): </b>${row["GDP \n($ USD billions PPP)"]}<br><b>GDP per capita in $ (PPP): </b>${row["GDP per capita in $ (PPP)"]}<br><b>health expenditure % of GDP: </b>${row["health expenditure \n% of GDP"]}<br><b>health expenditure per person: </b>${row["health expenditure \nper person"]}<br><b>unemployment (%): </b>${row["unemployment (%)"]}<br><b>Military Spending as % of GDP: </b>${row["Military Spending as % of GDP"]}`;
-            makeChart(row["indicator"], "GDP \n($ USD billions PPP)")
+            makeChart("GDP \n($ USD billions PPP)")
             var popupContent = `Name: ${row["indicator"]}<br>Lat: ${row["lat"]}<br>Long: ${row["long"]}`;
             var popup = L.popup().setLatLng(marker.getLatLng()).setContent(popupContent);
             popup.openOn(map);
@@ -109,45 +105,47 @@ makeMap()
 
 
 
-const makeChart = async(curr_country, metric) => {
-  const dataa = await loadData()
-  const ctx = document.getElementById('myChart');
-  myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: dataa.map(row => row.indicator),
-      datasets: [{
-        label: '# of Votes',
-        data: dataa.map(row => row[metric]),
-        borderWidth: 0,
-        backgroundColor: dataa.map(row => row.indicator === curr_country ? 'green' : '#c28ffe')
-      }]
-    },
-    options: {
-      animation: {
-        duration: 6000, 
-        easing: 'easeInOutQuad'
+const makeChart = async () => {
+  const items = ['GDP \n($ USD billions PPP)', 'GDP per capita in $ (PPP)', 'health expenditure \n% of GDP', 'health expenditure \nper person', 'unemployment (%)','Military Spending as % of GDP']
+  try {
+    const dataa = await loadData();
+    const ctx = document.getElementById('myChart');
+    myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: dataa.map(row => row.indicator),
+        datasets: [{
+          label: '# of Votes',
+          data: dataa.map(row => row[items[current_metric]]),
+          borderWidth: 0,
+          backgroundColor: dataa.map(row => row.indicator === current_country ? 'green' : '#c28ffe')
+        }]
       },
-      scales: {
-        y: {
-          type: "logarithmic",
-          beginAtZero: true
+      options: {
+        animation: {
+          duration: 0000,
+          easing: 'easeInOutQuad'
+        },
+        scales: {
+          y: {
+            type: "logarithmic",
+            beginAtZero: true
+          }
         }
       }
-    }
-  });
-}
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 
 // on change of dropdown
 const dropdown = document.getElementById('myDropdown');
-
 dropdown.addEventListener('change', (event) => {
   myChart.destroy();
-  const selectedValue = event.target.value;
-  console.log(selectedValue)
-  console.log(current_country)
-  makeChart(current_country, selectedValue)
+  current_metric = parseInt(event.target.value)
+  makeChart()
 });
 
 
