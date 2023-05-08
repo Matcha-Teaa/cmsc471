2cfcbbd9737a87db1ed65d39c20b6b0ae1df3be3
hsirkar <dhakal.rakrish@gmail.com>
2023-05-08 17:10:32 -0400

    wip

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Constants
-const latitudes = [33.93911, 41.153332, 28.033886, -11.202692, -38.416097, 40.069099, -25.274398, 47.516231, 40.143105, 25.03428, 26.0667, 23.684994, 13.193887, 53.709807, 50.503887, 17.189877, 9.30769, 27.514162, -16.290154, 43.915886, -22.328474, -14.235004, 4.535277, 42.733883, 12.238333, -3.373056, 16.002082, 12.565679, 7.369722, 56.130366, 6.611111, 15.454166, -35.675147, 35.86166, 4.570868, -11.875001, -4.038333, -0.228021, 9.748917, 7.539989, 45.1, 21.521757, 35.126413, 49.817492, 56.26392, 11.825138, 18.735693, -1.8312, 26.8206, 13.7942, 1.6508, 15.1794, 58.5953, -26.5225, 9.1450, -17.7134, 61.9241, 46.2276, -0.8037, 13.4432, 42.3154, 51.1657, 7.9465, 39.0742, 15.7835, 9.9456, 11.8037, 4.8604, 18.9712, 15.2000, 47.1625, 64.9631, 20.5937, -0.7893, 32.4279, 33.2232, 53.1424, 31.0461, 41.8719, 18.1096, 36.2048, 30.5852, 48.0196, 0.0236, 40.3399, 35.9078, 42.6026, 29.3117, 41.2044, 19.8563, 56.8796, 33.8547, -29.61, 6.4281, 26.3351, 54.6872, 49.8153, 41.6086, -18.7669, -13.2543, 4.2105, 3.2028, 17.5707, 35.9375, 21.0079, -20.3484, 23.6345, 47.4116, 46.8625, 42.7087, 31.7917, -18.6657, 21.9162, -22.9576, 28.3949, 52.1326, -40.9006, 12.8654, 17.6078, 9.082, 60.472, 21.4735, 30.3753, 8.5379, -6.314, -23.4425, -9.19, 12.8797, 51.9194, 39.3999, 25.3548, 45.9432, 61.524, -1.9403, 23.8859, 14.4974, 44.0165, 8.4606, 1.3521, 48.669, 46.1512, -9.6457, 5.1521, -30.5595, 6.877, 40.4168, 7.8731, 12.8628, 3.9193, 60.1282, 46.8182, 34.8021, 23.6978, 38.861, -6.369, 15.87, -8.8742, 8.6195, 10.6918, 33.8869, 38.9637, 38.9697, 1.3733, 48.3794, 23.4241, 55.3781, 37.0902, -32.5228, 41.3775, 6.4238, 14.0583, 15.5527, -13.1339, -19.0154]
-const longitudes = [67.709953, 20.168331, 1.659626, 17.873887, -63.616672, 45.038189, 133.775136, 14.550072, 47.576927, -77.39628, 50.5577, 90.356331, -59.543198, 27.953389, 4.469936, -88.49765, 2.315834, 90.433601, -63.588653, 17.679076, 24.684866, -51.92528, 114.727669, 25.48583, -1.561593, 29.918886, -24.013197, 104.990963, 12.354722, -106.346771, 20.939444, 18.732207, -71.542969, 104.195397, -74.297333, 43.872219, 21.758664, 15.827659, -83.753428, -5.54708, 15.2, -77.781167, 33.429859, 15.472962, 9.501785, 42.590275, -70.162651, -78.1834, 30.8025, -88.8965, 10.2679, 39.7823, 25.0136, 31.4659, 40.4897, 178.0650, 25.7482, 2.2137, 11.6094, -15.3101, 43.3569, 10.4515, -1.0232, 21.8243, -90.2308, -9.6966, -15.1804, -58.9302, -72.2852, -86.2419, 19.5033, -19.0208, 78.9629, 113.9213, 53.6880, 43.6793, -7.6921, 34.8516, 12.5674, -77.2975, 138.2529, 36.2384, 66.9237, 37.9062, 127.5101, 127.7669, 20.9030, 47.4818, 74.7661, 102.4955, 24.6032, 35.8623, 28.2336, -9.4295, 17.2283, 25.2797, 6.1296, 21.7453, 46.8691, 34.3015, 101.9758, 73.2207, -3.9962, 14.3754, -10.9408, 57.5522, -102.5528, 28.3699, 103.8467, 19.3744, -7.0926, 35.5296, 95.956, 18.4904, 84.124, 5.2913, 174.886, -85.2072, 8.0817, 8.6753, 8.4689, 55.9754, 69.3451, -80.7821, 143.9555, -58.4438, -75.0152, 121.774, 19.1451, -8.2245, 51.1839, 24.9668, 105.3188, 29.8739, 45.0792, -14.4524, 21.0059, -11.7799, 103.8198, 19.699, 14.9955, 160.1562, 46.1996, 22.9375, 31.307, -3.7038, 80.7718, 30.2176, -56.0278, 18.6435, 8.2275, 38.9968, 120.9605, 71.2761, 34.8888, 100.9925, 125.7275, 0.8248, -61.2225, 9.5375, 35.2433, 59.5563, 32.2903, 31.1656, 53.8478, -3.436, -95.7129, -55.7658, 64.5853, -66.5897, 108.2772, 48.5164, 27.8493, 29.1549]
+const years = {
+    'GDP \n($ USD billions PPP)': [2018, 2019, 2020, 2021],
+    'GDP per capita in $ (PPP)': [2018, 2019, 2020, 2021],
+    'health expenditure \n% of GDP': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+    'health expenditure \nper person': [2015, 2018, 2019],
+    'unemployment (%)': [2018, 2021],
+    'Military Spending as % of GDP': [2019, 2021]
+}
+const metrics = Object.keys(years);
 
 // Global variables
 let current_country = null;
@@ -8,64 +15,14 @@ let current_metric = 0;
 let myChart = null;
 let root = null;
 
-let myData = null;
+let myData = {};
 let countries = null;
 
-/*
-this function loads and cleans the data... replaces each '-' and '' in dataset with 0
-This function also adds the lat and long columns of every country
-*/
+// loads data and countries geojson
 const loadData = async () => {
-    let df = await d3.csv('data.csv');
-    df = df.slice(4, 178);
-    df = df.map((row, dex) => {
-        for (let key in row) {
-            if (row[key] === '-' || row[key] === '' || row[key] === '...') {
-                row[key] = '0';
-            }
-            if (row[key].includes('%')) {
-                row[key] = row[key].replace('%', '');
-            }
-        }
-        // add column name here as follows to convert to float
-        row['population'] = parseFloat(row['population']);
-        row['GDP per capita in $ (PPP)'] = parseFloat(row['GDP per capita in $ (PPP)']);
-        row['unemployment (%)'] = parseFloat(row['unemployment (%)']);
-        row['Military Spending as % of GDP'] = parseFloat(row['Military Spending as % of GDP']);
-        row['health expenditure \nper person'] = parseInt(row['health expenditure \nper person']);
-        row['health expenditure \n% of GDP'] = parseFloat(row['health expenditure \n% of GDP']);
-        row["GDP \n($ USD billions PPP)"] = parseFloat(row["GDP \n($ USD billions PPP)"]);
-        row["lat"] = latitudes[dex]
-        row["long"] = longitudes[dex]
-        return row;
-    });
-
-    myData = df;
-    countries = await d3.json("./countries.json");
-
-    console.log(df);
-    console.log(countries);
-
-    // uncomment this to get a csv of exactly what the data looks like after formatting
-    /*
-    const csvData = d3.csvFormat(df);
-    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("download", "cleaned_data.csv");
-    link.style.visibility = 'hidden';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    */
+    myData = await d3.csv("./data-processed.csv");
+    countries = await d3.json("./countries.json")
 }
-
-// prints data array to console
-loadData().then(data => {
-    console.log(data);
-    myData = data;
-});
 
 // generates the leaflet map and plots all the points
 // const makeMap = async () => {
@@ -118,17 +75,48 @@ loadData().then(data => {
 // }
 // makeMap()
 
+let leafletMaps = [];
 
 // Adds maps to page
 const updateMaps = async () => {
-    const maps = d3.select(".row.map")
-        .selectAll("map")
-        .data()
+    // Select the container element for the maps
+    const mapRow = d3.select(".row.map");
+
+    // Remove all existing elements
+    mapRow.html("");
+    leafletMaps = [];
+
+    // Get the data for the current metric and years
+    const data = years[metrics[current_metric]];
+
+    // Bind the data to the existing map elements
+    const maps = mapRow.selectAll("#map")
+        .data(data, d => (d, current_metric));
+
+    // Add new map elements for new data items
+    maps.enter()
+        .append("div")
+        .text(d => d.toString())
+        .append("div")
+        .attr("id", "map")
+        .each(function (d) {
+            const map = L.map(this).setView([51.505, -0.09], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                maxZoom: 18,
+            }).addTo(map);
+            leafletMaps.push(map);
+        })
+
+    leafletMaps.forEach(m1 => {
+        leafletMaps.forEach(m2 => {
+            m1.sync(m2);
+        })
+    })
 }
 
-
+// creates the bar chart
 const makeChart = async () => {
-    const items = ['GDP \n($ USD billions PPP)', 'GDP per capita in $ (PPP)', 'health expenditure \n% of GDP', 'health expenditure \nper person', 'unemployment (%)', 'Military Spending as % of GDP']
     try {
         const dataa = await loadData();
         const ctx = document.getElementById('myChart');
@@ -161,20 +149,7 @@ const makeChart = async () => {
     }
 };
 
-
-// on change of dropdown
-const dropdown = document.getElementById('myDropdown');
-dropdown.addEventListener('change', (event) => {
-    myChart.destroy();
-    root.dispose()
-    // jump1
-    current_metric = parseInt(event.target.value)
-    makeChart()
-    // createTreemap()
-});
-
-
-
+// creates the tree map
 const createTreemap = async () => {
     const dataa = await loadData();
     const items = ['GDP \n($ USD billions PPP)', 'GDP per capita in $ (PPP)', 'health expenditure \n% of GDP', 'health expenditure \nper person', 'unemployment (%)', 'Military Spending as % of GDP']
@@ -215,4 +190,21 @@ const createTreemap = async () => {
     series.appear(10000, 1000);
 }
 
+// on change of dropdown
+const dropdown = document.getElementById('myDropdown');
+dropdown.addEventListener('change', event => {
+    // console.log(event.target.value);
+    // myChart.destroy();
+    // root.dispose()
+    // // jump1
+    current_metric = parseInt(event.target.value);
+    updateMaps();
+    // makeChart()
+    // // createTreemap()
+});
 
+// initial call
+loadData().then(() => {
+    // console.log(data);
+    updateMaps();
+});
